c9cfc7ad0f0334b4c2187ecc21577c81c53181f4
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2023-02-19 01:06:25 +0600

    Add script

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const keyCode = document.getElementById("keycode");
+const key = document.getElementById("key");
+
+window.addEventListener("keydown", function (e) {
+    if (e.keyCode == 32) {
+        keyCode.innerHTML = `<p>${e.keyCode}</p>`;
+        key.innerHTML = `<p>space</p>`;
+    } else {
+        keyCode.innerHTML = `<p>${e.keyCode}</p>`;
+        key.innerHTML = `<p>${e.key}</p>`;
+    }
+});
